beaa2677159e613dae703c8e9f62db9c629238d2
csj <chris.stjohn@gmail.com>
2018-04-06 21:25:14 -0400

    const

--- src/main/kotlin/com/codingame/game/Constants.kt
+++ src/main/kotlin/com/codingame/game/Constants.kt
@@ -1,43 +1,43 @@
 package com.codingame.game
 
 object Constants {
-  val STARTING_GOLD = 100
+  const val STARTING_GOLD = 100
 
-  val QUEEN_SPEED = 60
-  val TOWER_HP_INITIAL = 200
-  val TOWER_HP_INCREMENT = 100
-  val TOWER_HP_MAXIMUM = 800
-  val TOWER_CREEP_DAMAGE_MIN = 5
-  val TOWER_CREEP_DAMAGE_CLIMB_DISTANCE = 200
-  val TOWER_QUEEN_DAMAGE_MIN = 1
-  val TOWER_QUEEN_DAMAGE_CLIMB_DISTANCE = 200
-  val TOWER_MELT_RATE = 4
-  val TOWER_COVERAGE_PER_HP = 1000
+  const val QUEEN_SPEED = 60
+  const val TOWER_HP_INITIAL = 200
+  const val TOWER_HP_INCREMENT = 100
+  const val TOWER_HP_MAXIMUM = 800
+  const val TOWER_CREEP_DAMAGE_MIN = 5
+  const val TOWER_CREEP_DAMAGE_CLIMB_DISTANCE = 200
+  const val TOWER_QUEEN_DAMAGE_MIN = 1
+  const val TOWER_QUEEN_DAMAGE_CLIMB_DISTANCE = 200
+  const val TOWER_MELT_RATE = 4
+  const val TOWER_COVERAGE_PER_HP = 1000
 
-  val GIANT_BUST_RATE = 80
+  const val GIANT_BUST_RATE = 80
 
-  val OBSTACLE_GAP = 90
+  const val OBSTACLE_GAP = 90
   val OBSTACLE_RADIUS_RANGE = 60..100
   val OBSTACLE_GOLD_RANGE = 200..250
   val OBSTACLE_MINE_BASESIZE_RANGE = 1..3
-  val OBSTACLE_GOLD_INCREASE = 50
-  val OBSTACLE_GOLD_INCREASE_DISTANCE_1 = 500
-  val OBSTACLE_GOLD_INCREASE_DISTANCE_2 = 200
-  val OBSTACLE_PAIRS = 12
+  const val OBSTACLE_GOLD_INCREASE = 50
+  const val OBSTACLE_GOLD_INCREASE_DISTANCE_1 = 500
+  const val OBSTACLE_GOLD_INCREASE_DISTANCE_2 = 200
+  const val OBSTACLE_PAIRS = 12
 
-  val MELEE_DAMAGE = 1
-  val RANGED_DAMAGE = 2
+  const val MELEE_DAMAGE = 1
+  const val RANGED_DAMAGE = 2
 
-  val QUEEN_RADIUS = 30
-  val QUEEN_MASS = 10000
-  val QUEEN_HP = 200
-  val QUEEN_VISION = 300
+  const val QUEEN_RADIUS = 30
+  const val QUEEN_MASS = 10000
+  const val QUEEN_HP = 200
+  const val QUEEN_VISION = 300
 
   val WORLD_WIDTH = viewportX.last - viewportX.first
   val WORLD_HEIGHT = viewportY.last - viewportY.first
 
-  val TOUCHING_DELTA = 5
-  val WOOD_FIXED_INCOME = 10
+  const val TOUCHING_DELTA = 5
+  const val WOOD_FIXED_INCOME = 10
 }
 
 object Leagues {
