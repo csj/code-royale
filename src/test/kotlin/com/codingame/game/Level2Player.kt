d44a07649236ca5b144fd8f22b59212e33ae2a21
Netbattler11 <Netbattler11@gmail.com>
2018-04-17 20:14:38 -0500

    Fixed Level1 and Level2 Player names

--- src/test/kotlin/com/codingame/game/Level2Player.kt
+++ src/test/kotlin/com/codingame/game/Level2Player.kt
@@ -10,23 +10,23 @@ class Level2Player(stdin: InputStream, stdout: PrintStream, stderr: PrintStream)
   init {
 
     while (true) {
-      val (queenLoc, _, _, touch, _, _, obstacles, _, _) = readInputs()
-//      stderr.println("Touching: $touch")
-      fun getQueenAction(): String {
+      val (queenLoc, _, _, _, _, _, obstacles, _, _) = readInputs()
 
+      fun getQueenAction(): String {
         val queenTarget = obstacles
-          .filter { it.owner == -1 }
+          .filter { it.owner == -1 || (it.owner == 0 && it.structureType == 1 && it.incomeRateOrHealthOrCooldown < 400) }
           .minBy { it.location.distanceTo(queenLoc) } ?: return "WAIT"
 
-        val needsMelee = obstacles.count { it.structureType == 2 && it.owner == 0 && it.attackRadiusOrCreepType == 0 } < 2
+        val needsMelee = !obstacles.any { it.structureType == 2 && it.owner == 0 && it.attackRadiusOrCreepType == 0 }
         val needsRanged = !obstacles.any { it.structureType == 2 && it.owner == 0 && it.attackRadiusOrCreepType == 1 }
-//        val needsGiant = !obstacles.any { it.structureType == 2 && it.owner == 0 && it.attackRadiusOrCreepType == 2 }
+        val needsGiant = !obstacles.any { it.structureType == 2 && it.owner == 0 && it.attackRadiusOrCreepType == 2 }
+        val needsTower = obstacles.count { it.structureType == 1 && it.owner == 0 } < 3
 
         return when {
           needsMelee -> "BUILD ${queenTarget.obstacleId} BARRACKS-MELEE"
           needsRanged -> "BUILD ${queenTarget.obstacleId} BARRACKS-RANGED"
-//          needsGiant -> "BUILD ${queenTarget.obstacleId} BARRACKS-GIANT"
-//          else -> "BUILD ${queenTarget.obstacleId} TOWER"
+          needsGiant -> "BUILD ${queenTarget.obstacleId} BARRACKS-GIANT"
+          needsTower -> "BUILD ${queenTarget.obstacleId} TOWER"
           else -> "WAIT"
         }
       }
