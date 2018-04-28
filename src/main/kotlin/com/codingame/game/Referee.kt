8a63cd9a147a0d1a9e0b82170fb8df86a14b0988
iadevoops <31867904+iadevoops@users.noreply.github.com>
2018-04-28 17:56:49 +0300

    Fix units start point outside barracks

--- src/main/kotlin/com/codingame/game/Referee.kt
+++ src/main/kotlin/com/codingame/game/Referee.kt
@@ -205,7 +205,9 @@ class Referee : AbstractReferee() {
                     CreepType.GIANT ->
                       GiantCreep(barracks.owner, barracks.creepType, obstacles)
                   }.also {
-                    it.location = barracks.obstacle.location + Vector2(iter, iter)
+                    val c = if (barracks.owner.isSecondPlayer) -1 else 1
+                    it.location = barracks.obstacle.location + Vector2(c * iter, c * iter)
+                    //it.location = barracks.obstacle.location + Vector2(iter, iter) // Fix units start point outside barracks
                     it.finalizeFrame()
                     it.location = it.location.towards(barracks.owner.enemyPlayer.queenUnit.location, 30.0)
                     it.finalizeFrame()
